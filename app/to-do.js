397cb758658c104a4e072d205bfdcee4769bb0bb
Jesse <rosecity27@comcast.net>
2017-01-19 10:52:48 -0800

    add for loop

--- app/to-do.js
+++ app/to-do.js
@@ -7,6 +7,12 @@ var Task = (function () {
     return Task;
 }());
 var tasks = [];
+tasks.push(new Task('Buy chocolate.', 'Low'));
 tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Do laundry.', 'High'));
 console.log(tasks);
 // Note that TypeScript requires you define strings with single quotes('string!'). You will receive errors if your strings contain double quotes ("string!").
+
+// Because we've made changes to our .ts file, we have to re-compile to see them reflected in the browser:
+
+// $ tsc app/to-do.ts  (compiler command)
--- app/to-do.ts
+++ app/to-do.ts
@@ -1,18 +1,23 @@
 class Task {
-  done: boolean;
-  description: string;
-  priority: string;
+  done: boolean = false;
 
-  constructor(descriptionParameter: string, priorityParameter: string){
-   this.done = false;
-   this.description = descriptionParameter;
-   this.priority = priorityParameter;
-  }
+  constructor(public description: string, public priority: string){}
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('Do the dishes.', 'Medium'));
 console.log(tasks);
 
+for(var task of tasks) {
+  console.log(task);
+}
+
 
 // Note that TypeScript requires you define strings with single quotes('string!'). You will receive errors if your strings contain double quotes ("string!").
+
+
+// If constructor() parameters share the exact same name as class properties (like description, or priority in the example above), TypeScript will automatically assign them to a public property of the same name and type.
+
+// class Thing {
+//   propertyName: type = defaultValue;
+// }
